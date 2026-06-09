ea892ea1fbe4831f06ab20f24c448bcd6335cca6
Fusion831 <dakshdaksh698@gmail.com>
2026-06-09 20:13:07 +0000

    Created a FastAPI server and a symbolic health checkpoint rather to showcase the commitment

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def main():
-    print("Hello from bettercalllaw!")
+from fastapi import FastAPI
 
+app = FastAPI()
+
+
+@app.get("/health")
+def health() -> dict:
+    return {"This just serves as a commitment message that I will end up finishing the app and it is currently working, with my own fingers" : "."}
 
 if __name__ == "__main__":
     main()
