ee48cc8b97189fa981eeeb6075a3ed07493b799d
Fusion831 <dakshdaksh698@gmail.com>
2026-06-11 04:43:26 +0000

    I finished the Ingestion pipeline and actually fixed a few errors as well, uuid was being done out of loop, so my qdrant embedding kept being rewrriten continuosly so switched it onto the loop, and then switched from a for loop to while loop, as the index wasnt being properly updated

--- database/ingestion.py
+++ database/ingestion.py
@@ -1,11 +1,22 @@
 from docling.document_converter import DocumentConverter
 from typing import List
 from sentence_transformers import SentenceTransformer
+from qdrant_client import QdrantClient
+from qdrant_client.models import Distance, VectorParams
+from qdrant_client.models import PointStruct
+from qdrant_client.models import Filter, FieldCondition, MatchValue
+import uuid
+
+
 
 model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
-
+client = QdrantClient(":memory:")
 
 source = "./RandomFile.pdf"  # file path or URL
+client.create_collection(
+    "manual_Ingestion",
+    vectors_config = VectorParams(size = 384, distance = Distance.DOT)
+)
 converter = DocumentConverter() 
 doc = converter.convert(source).document
 
@@ -18,24 +29,49 @@ def chunks(doc : str,chunk_size : int, overlap: int) -> List[str]:
     #My approach is to have a pointer to manage the chunks size to string them, append the string, then afterwards, we decrease 
     #pointer by 50 in it. 
     start = 0
-    for i in range(len(doc)):
-        if(i + chunk_size >= len(doc)):
-            chunks.append(doc[i:])
-            return chunks
-        currChunk = doc[i:i+chunk_size]
-        i = chunk_size - overlap
-        chunks.append(currChunk)
+    while start < len(doc):
+        chunk = doc[start:start+chunk_size]
+        chunks.append(chunk)
+        start += (chunk_size - overlap)
         
     return chunks
 #Size 384 for embeddings
-def embeddings(chunks: List[str]):
+def embeddings(chunks: List[str]) -> List[List[float]]:
     embeds = []
     for chunk in chunks:
         embeds.append(model.encode(chunk))
     return embeds
 
 chunking = chunks(data,300,50)
-modelEmbeddings = embeddings
+modelEmbeddings = embeddings(chunking)
+print(f"Length of Embeddings: {len(modelEmbeddings[0])}")
 
+
+Points = []
+
+for chunk,embedding in zip(chunking,modelEmbeddings):
+    my_uuid = uuid.uuid4()
+    id = my_uuid.int
+    Points.append(PointStruct(id = id, vector = embedding,payload = {"text":chunk} ))
+
+    
+client.upsert(
+    collection_name = "manual_Ingestion",
+    wait = True,
+    points = Points,
+    
+)
+
+Query = "What is a distributed Database?"
+query = model.encode(Query)
+
+search_result = client.query_points(
+    collection_name = "manual_Ingestion",
+    query = query,
+    with_payload = True,
+    limit = 3,
+).points
+
+print(search_result)
 
 
