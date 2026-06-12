87361945b86a79528e9243580e6a30b2d3369577
Fusion831 <dakshdaksh698@gmail.com>
2026-06-12 09:24:33 +0000

    Policy Upload endpoint created, I now have to create functions, where langchain receives the markdown data for Parsing it into section to explain it, and chunks are given to a qdrant client, to get it for futher queries, and understanding The exact coverage and exclusions in the policy

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+from policy.fileIngestion import  parse_markdown, parse_headers
+import os
 
 app = FastAPI()
 
@@ -6,6 +9,35 @@ app = FastAPI()
 @app.get("/health")
 def health() -> dict:
     return {"This just serves as a commitment message that I will end up finishing the app and it is currently working, with my own fingers" : "."}
+
+@app.get("/upload-policy")
+async def policyHandling(file: UploadFile = File(...)):
+    tempFilePath = f"temp_{file.filename}"
+    try:
+        with open(tempFilePath, "wb") as buffer: 
+            buffer.write(await file.read())
+        markDownData = await parse_markdown(tempFilePath)
+        chunks = await parse_headers(tempFilePath)
+        responseData = []
+        for chunk in chunks:
+            responseData.append(
+                {
+                    "metadata" : chunk.metadata,
+                    "text": chunk.page_content[:200]
+                }
+            )
+        return {
+            "status": "success",
+            "filename": file.filename,
+            "total_chunks": len(chunks),
+            "data": responseData
+        }
+    except Exception as e:
+        return {"status" : "error", "message": str(e)}
+    finally:
+        if os.path.exists(tempFilePath):
+            os.remove(tempFilePath)   
+        
 
 if __name__ == "__main__":
     main()
