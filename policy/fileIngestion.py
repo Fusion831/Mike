b44c9f2c16e4005f6efb2cfab8dfc2eb4a032a35
Fusion831 <dakshdaksh698@gmail.com>
2026-06-12 09:07:25 +0000

    Created The basic parsing using asynchronous functions and langchain markdown text splitter

---
+++ policy/fileIngestion.py
@@ -0,0 +1,25 @@
+from docling.document_converter import DocumentConverter
+from langchain_text_splitters import MarkdownHeaderTextSplitter
+from typing import List
+from langchain_core.documents import Document
+
+
+
+
+async def parse_markdown(source):
+    converter = DocumentConverter()
+    doc = converter.convert(source).document
+    data = doc.export_to_markdown()
+    return data
+
+async def parse_headers(markdown) -> List[Document]:
+    headers_to_split_on = [
+    ("#", "Chapter"),       
+    ("##", "Section"),      
+    ("###", "Subsection"),  
+    ]
+    markdownSplitter = MarkdownHeaderTextSplitter(
+        headers_to_split_on = headers_to_split_on,
+    )
+    MarkdownChunks = markdownSplitter.split_text(markdown)
+    return MarkdownChunks
